b7273f3a0d6ff7b244fadea67363558ec36d32d2
Mr.Shtein <mr.shtein61@yandex.ru>
2021-01-30 15:13:13 +0300

    Added Encryptor and its methods

---
+++ encryptdecrypt/Encryptor.java
@@ -0,0 +1,46 @@
+package encryptdecrypt;
+
+import java.util.HashMap;
+
+public class Encryptor {
+
+    private HashMap<Character, Character> letters;
+
+    public Encryptor() {
+        this.letters = addLetters();
+    }
+
+    public HashMap<Character, Character> addLetters() {
+        HashMap<Character, Character> mapOfLetters = new HashMap<>();
+        for (char start = 'a', stop = 'z'; start <= 'z'; start++, stop--) {
+            mapOfLetters.put(start, stop);
+        }
+        return mapOfLetters;
+    }
+
+    public String encryptPhrase(String phraseToEncrypt) {
+        char[] charArray = phraseToEncrypt.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char item : charArray) {
+            if (item == '!') {
+                sb.append('!');
+            } else if (item == ' ') {
+                sb.append(' ');
+            } else {
+                sb.append(letters.get(item));
+            }
+        }
+        return sb.toString();
+    }
+
+    public void hidePhrase(String phraseToHide) {
+        for (int i = 0; i < phraseToHide.length(); i++) {
+            char currentChar = phraseToHide.charAt(i);
+            if (Character.isLetter(currentChar)) {
+                System.out.print("#");
+            } else {
+                System.out.print(currentChar);
+            }
+        }
+    }
+}
